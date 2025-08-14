ce9e0756195962602e9fedb32d70239a1182fff3
imam <imamalbana123@gmail.com>
2025-08-14 09:34:16 +0700

    Done Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-amber-600">React TODO</h1>
+    </>
+  );
+}
+
+export default App;
